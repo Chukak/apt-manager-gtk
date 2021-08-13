8e29f0bb161fe156ee5be47b4962bede480d76a8
Sergey Fedorov <chukak12@gmail.com>
2021-08-13 19:28:45 +0300

    Preparing and configuring the GTKMM library for future use
    Compiling resources.
    Load resources.
    Moved cache.cpp and textacquirestatus.cpp to the package namespace.
    Opening the main window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
-#include "cache.h"
+#include "package/cache.h"
 #include "utils.h"
+
+#include <gtkmm/application.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/applicationwindow.h>
+#include <giomm/resource.h>
 
 int main(int argc, char** argv)
 {
 	(void)argc;
 	(void)argv;
 
+	// load resources
+	Glib::RefPtr<Gio::Resource> resource_bundle =
+		Gio::Resource::create_from_file(UI_RESOURCE_FILE);
+	resource_bundle->register_global();
+
+	Glib::RefPtr<Gtk::Application> app(Gtk::Application::create(argc, argv));
+	Gtk::ApplicationWindow* appWin;
+
+	Glib::RefPtr<Gtk::Builder> ui(Gtk::Builder::create_from_resource("/ui/main.ui"));
+	ui->get_widget<Gtk::ApplicationWindow>("Main", appWin);
+
+	/*
 	package::Cache cache;
 	package::CandidateList candidates = cache.getCandidates(package::Upgradable);
 	utils::GetLog() << candidates << std::endl;
+	*/
 
-	return 0;
+	return app->run(*appWin);
 }
